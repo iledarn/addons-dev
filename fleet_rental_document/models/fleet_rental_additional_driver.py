64fa4fb7dd16e44f98e3616ff3bdcd81b1b0682a
Ildar Nasyrov <nasyrov@it-projects.info>
2016-08-03 18:00:55 +0500

    [FIX] fleet_rental_document: all fields of additional driver are optional on document

--- fleet_rental_document/models/fleet_rental_additional_driver.py
+++ fleet_rental_document/models/fleet_rental_additional_driver.py
@@ -6,4 +6,4 @@ class FleetRentalAdditionalDriver(models.Model):
     _inherit = 'personal.drive_license'
     _name = 'fleet_rental.additional_driver'
 
-    name = fields.Char(string='Driver Name', required=True)
+    name = fields.Char(string='Driver Name')
